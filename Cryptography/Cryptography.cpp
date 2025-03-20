a87f05a989c60d26ef721b00f70a534d2e094c0c
ashrali <stunner437@gmail.com>
2025-03-20 19:24:40 +0500

    Key added

--- Cryptography/Cryptography.cpp
+++ Cryptography/Cryptography.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 int main()
 {
@@ -8,6 +9,7 @@ int main()
       std::getline(std::cin, str);
       std::cout << "Enter shift value : ";
       std::cin >> shift;
+      std::cin.ignore();
       std::string encrypteddata = "";
       for (int i = 0; i < str.length(); i++)
       {
@@ -28,7 +30,17 @@ int main()
                   encrypteddata += str[i];
             }
       }
-      std::cout << "Encrypted data : " << str << "\n";
-      std::cout << "Decrypted data : " << encrypteddata;
+      std::string key;
+      std::cout << "Enter key : ";
+      std::getline(std::cin, key);
+      if (key == "1234")
+      {
+            std::cout << "Decrypted data : " << str << "\n";
+      }
+      else
+      {
+            std::reverse(encrypteddata.begin(), encrypteddata.end());
+            std::cout << "Encrypted data : " << encrypteddata;
+      }
       return 0;
 }
